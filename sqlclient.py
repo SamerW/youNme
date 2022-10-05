3e79bb1b2d1ce56e95b6a3f31f44c0f488472ecf
Siem Hadish <siemghirmai2@gmail.com>
2022-10-05 19:35:43 +0400

    10/5 2

--- sqlclient.py
+++ sqlclient.py
@@ -1,12 +1,32 @@
-from sqlmodel import SQLModel, create_engine
+# from sqlmodel import SQLModel, create_engine\
+import pickle
+import rsa
+import base64
 
-sqlite_file_name = "database.db"
-sqlite_url = f"sqlite:///{sqlite_file_name}"
+# sqlite_file_name = "database.db"
+# sqlite_url = f"sqlite:///{sqlite_file_name}"
 
-engine = create_engine(sqlite_url)
+# engine = create_engine(sqlite_url)
 
 
 
-def create_db_and_tables():
-    SQLModel.metadata.create_all(engine)
+# def create_db_and_tables():
+#     SQLModel.metadata.create_all(engine)
+    
+    
+def getServerKeys():
+    import requests
+
+    headers = {
+        'accept': 'application/json',
+    }
+
+    response = requests.get('http://127.0.0.1:8000/getserversk', headers=headers)
+    if response.status_code == 200:
+        res = response.json()
+        ser_pubK = pickle.loads(base64.b64decode(res['Server_public_K']))
+        return ser_pubK
+        
+        
+
     
